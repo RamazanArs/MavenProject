10b6b4b16bd06347581ad23682025d353d935a38
RamazanArs <ramazan.arslan11@gmail.com>
2022-01-19 12:25:14 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Github Repo is already published to Github");
+        System.out.println("Doing more work");
 
     }
 }
